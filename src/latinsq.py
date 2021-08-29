42b7c60387d31dcfa7ec9098a540739a2a9fd86f
Ian Allison <iana@Ians-MacBook-Air.local>
2021-08-28 18:14:25 -0700

    Started adding permutation generator

--- src/latinsq.py
+++ src/latinsq.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+
 class LatinSquare(object):
     """
     A latin square object
 
-    Internally, we represent an n x n latin square as a 3 x n**2 array, specifying
-    the row index, column index and value for each position. This makes some of the
-    solver manipulations significanlty easier. For example
+    Internally, we represent an n x n latin square as a 3 x n**2 array,
+    specifying the row index, column index and value for each position. This
+    makes some of the solver manipulations significanlty easier. For example
 
       1 3 2
       2 1 3
@@ -17,25 +18,29 @@ class LatinSquare(object):
       R: 111222333
       C: 123123123
       V: 132213321
-    
-    Which should be read as the value in rown 1, position 2 has value 3 etc. (we will 
-    index from 1 throughout)
+
+    Which should be read as the value in rown 1, position 2 has value 3 etc.
+    (we will index from 1 throughout)
     """
-    def __init__(self, square = None, n=3):
+    def __init__(self, square=None, n=3):
         if square:
             assert type(square) == np.ndarray
             assert len(square.shape) == 2
-            assert square.shape[0] == share.shape[1]
+            assert square.shape[0] == square.shape[1]
             self.n = square.shape[0]
             self.square = square
         else:
             self.n = n
-            self.square = self.random(self.n)
+            self.random()
 
-    
     def __repr__(self):
-        return self.square.__repr__()
+        return repr(self.square)
 
+    def __getitem__(self, idx):
+        return self.square[idx[0], idx[1]]
+
+    def __setitem__(self, idx, value):
+        self.square[idx[0], idx[1]] = value
 
     def to_rcv(self):
         r = np.repeat(np.arange(1, self.n + 1), self.n)
@@ -49,28 +54,39 @@ class LatinSquare(object):
         s = s * np.ones(self.n, dtype=np.int8)[:, np.newaxis] + 1
 
         return (
-            np.all(np.sort(self.square, axis=0) == s.T) and 
+            np.all(np.sort(self.square, axis=0) == s.T) and
             np.all(np.sort(self.square, axis=1) == s)
         )
-    
+
     def from_rcv(self, r, c, v):
         r = np.array(r)
         c = np.array(c)
         v = np.array(v)
         self.square = np.ones((3, 3), dtype=np.int8) * -1
-        self.square[r.reshape(3, 3) - 1, c.reshape(3, 3) - 1] = v.reshape(3,3)
+        self.square[r.reshape(3, 3) - 1, c.reshape(3, 3) - 1] = v.reshape(3, 3)
 
-
-    def random(self, n):
+    def random(self):
         """
         Generate a random latin square
 
         Use MCMC to sample from the space of valid latin squares
         """
-        square = np.ones((n, n), dtype=np.int8) * -1
-        return square
+        self.square = np.ones((self.n, self.n), dtype=np.int8) * -1
+        for idx, row in enumerate(self.square):
+            self.square[idx, :] = np.roll(np.arange(1, self.n + 1), idx)
+
+        # Just a shuffle, not really random, result is in same
+        rng = np.random.default_rng()
+        self.square = self.square[rng.permutation(self.n)]
+        self.square = self.square[:, rng.permutation(self.n)]
+
+        # Generate the equivalent 3D incidence matrix
+        xy = np.mgrid[0:self.n, 0:self.n]
+        idx = np.array([xy[0], xy[1], self.square - 1])
+        self._i_matrix = np.zeros((self.n, self.n, self.n), dtype=np.int8)
+        self._i_matrix[idx[2], idx[0], idx[1]] = 1
 
 
 if __name__ == "__main__":
-    l = LatinSquare()
-    print("Latin Square: ", l)
+    sq = LatinSquare()
+    print("Latin Square: ", sq)
